b65037347018b7a0baf7ad5a495123b3bfdb4b19
Raghav Aggarwal <raghavaggsss@gmail.com>
2020-08-09 21:14:20 -0700

    Queues based on data

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,7 +11,7 @@ public class CameraScript : MonoBehaviour
     public Transform personPos;
     public GameObject person;
     public Queue<GameObject> personQueue;
-    private float xSpace = 1f;
+    private float xSpace = 0.5f;
     public GameObject elevatorObj;
     public GameObjectTransition eleTransition;
     private ElevatorScript elevatorScript;
@@ -21,11 +21,14 @@ public class CameraScript : MonoBehaviour
     private List<GameObjectTransition> personTransitionList;
     private List<Event> eventList;
     private List<Event> eleEventList;
-    private Queue<GameObject>[] eleQueueUp;
-    private Queue<GameObject>[] eleQueueDown;
-    private Queue<GameObject>[] clinicQueue;
-    private Queue<GameObject>[] doctorQueue;
+    private QueueObj[] eleQueueUp;
+    private QueueObj[] eleQueueDown;
+    private QueueObj[] doctorQueue;
+    private QueueObj[] doctorVisited;
     private float elapsedTime = 0;
+    public float yLimit = 3f;
+    public int numFloors = 9;
+    public float sizeFloor;
 
 
     private void Awake()
@@ -41,6 +44,11 @@ public class CameraScript : MonoBehaviour
         personTransitionList = new List<GameObjectTransition>();
         eventList = CSVReader.Read("DataCSV");
         eleEventList = eventList.Where(e => e.eventName == EventName.elevator_load).ToList();
+        sizeFloor = 2 * yLimit / numFloors;
+        eleQueueUp = initQueue(-Vector3.right * xSpace);
+        eleQueueDown = initQueue(-Vector3.right * xSpace*8);
+        doctorQueue = initQueue(Vector3.right * xSpace*8);
+        doctorVisited = initQueue(Vector3.right * xSpace * 16);
     }
     // Start is called before the first frame update
     void Start()
@@ -53,6 +61,12 @@ public class CameraScript : MonoBehaviour
     {
         elapsedTime += Time.deltaTime;
         transitionHelper();
+        // Testing only
+        if (Input.GetKeyUp(KeyCode.U)) updateQueue(doctorQueue[0], doctorQueue[0].q.Count + 1);
+        if (Input.GetKeyUp(KeyCode.L)) updateQueue(doctorQueue[0], 2);
+        if (Input.GetKeyUp(KeyCode.R)) {
+            updateQueue(doctorQueue[6], doctorQueue[6].q.Count + 4);
+        }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
@@ -96,6 +110,7 @@ public class CameraScript : MonoBehaviour
         personTransitionList.RemoveAll(got => !got.transitionX());
         pushEleTransition();
         eleTransition.transitionY();
+        pushPersonTransition();
     }
 
     private void pushEleTransition()
@@ -106,11 +121,78 @@ public class CameraScript : MonoBehaviour
             Event curEvent = eleEventList[1];
             if (elapsedTime > prevEvent.time)
             {
-                eleTransition.dest = new Vector3(elevatorObj.transform.position.x, -elevatorScript.yLimit +
-                    elevatorScript.sizeFloor * curEvent.floorNum, elevatorObj.transform.position.z);
+                eleTransition.dest = new Vector3(elevatorObj.transform.position.x, -yLimit +
+                    sizeFloor * curEvent.floorNum, elevatorObj.transform.position.z);
                 eleTransition.transitionTime = curEvent.time - prevEvent.time;
                 eleTransition.transitionSpeed = Vector3.Distance(eleTransition.dest, elevatorObj.transform.position) / eleTransition.transitionTime;
                 eleEventList.RemoveAt(0);
+            }
+        }
+    }
+    private QueueObj[] initQueue(Vector3 xoffSet)
+    {
+        QueueObj[] q = new QueueObj[elevatorScript.numFloors];
+        for (int i = 0; i < elevatorScript.numFloors; i++)
+        {
+            q[i] = new QueueObj(personPos.position + Vector3.up*(i*sizeFloor)+ xoffSet);
+        }
+        return q;
+    }
+    private void updateQueue(QueueObj queueObj, int newSize)
+    {
+        int sizeDiff = Mathf.Abs(newSize - queueObj.q.Count);
+        if (newSize > queueObj.q.Count)
+        {
+            for (int i = 0; i < sizeDiff; i++)
+            {
+                GameObject currCicle = Instantiate(person);
+                currCicle.SetActive(true);
+                currCicle.transform.position = queueObj.offset - Vector3.right * xSpace * queueObj.q.Count;
+                currCicle.GetComponent<Renderer>().material.SetColor("_Color", Random.ColorHSV());
+                PersonScript currCircleScript = currCicle.GetComponent<PersonScript>();
+                queueObj.q.Enqueue(currCicle);
+            }
+        }
+        if (newSize < queueObj.q.Count)
+        {
+            for (int i = 0; i < sizeDiff; i++)
+            {
+                lastPerson = queueObj.q.Peek();
+                lastPersonScript = lastPerson.GetComponent<PersonScript>();
+                Destroy(lastPerson);
+                //lastPerson.transform.position += xSpace * Vector3.right;
+                //lastPerson.transform.SetParent(elevatorObj.transform);
+                queueObj.q.Dequeue();
+            }
+            
+            foreach (GameObject curr in queueObj.q)
+            {
+                personTransitionList.Add(new GameObjectTransition(curr, curr.transform.position + sizeDiff*Vector3.right * xSpace));
+            }
+        }
+        else return;
+    }
+    private void pushPersonTransition()
+    {
+        if (eventList.Count > 0)
+        {
+            Event curEvent = eventList[0];
+            if (curEvent.time < elapsedTime || Mathf.Approximately(curEvent.time, elapsedTime))
+            {
+                switch (curEvent.eventName)
+                {
+                    case EventName.hall_queue:
+                        if (curEvent.eleDir == EleDirection.UP) updateQueue(eleQueueUp[curEvent.floorNum], curEvent.newVal);
+                        else if (curEvent.eleDir == EleDirection.DOWN) updateQueue(eleQueueDown[curEvent.floorNum], curEvent.newVal);
+                        break;
+                    case EventName.doctor_queue:
+                        updateQueue(doctorQueue[curEvent.floorNum], curEvent.newVal);
+                        break;
+                    case EventName.doctor_visited:
+                        updateQueue(doctorVisited[curEvent.floorNum], curEvent.newVal);
+                        break;
+                }
+                eventList.RemoveAt(0);
             }
         }
     }
---
+++ Assets/Scripts/QueueObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueueObj
+{
+    public Vector3 offset;
+    public Queue<GameObject> q;
+
+    public QueueObj(Vector3 offset)
+    {
+        this.offset = offset;
+        q = new Queue<GameObject>();
+    }
+}
