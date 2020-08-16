664cb2b805833b956c69df9ef505bad256e8e250
Raghav Aggarwal <raghavaggsss@gmail.com>
2020-08-16 05:13:10 -0700

    Play/ pause button

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class ButtonScript : MonoBehaviour
+{
+    private float prevTimeScale = 1f;
+    public Button playPauseButton;
+    public Sprite playImage;
+    public Sprite pauseImage;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void onFastForward()
+    {
+        Time.timeScale += 0.5f;
+        prevTimeScale = Time.timeScale;
+    }
+
+    public void onSlowMo()
+    {
+        Time.timeScale = Mathf.Max(0.1f, Time.timeScale - 0.5f);
+        prevTimeScale = Time.timeScale;
+    }
+
+    public void onPlayPause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = prevTimeScale;
+            playPauseButton.image.sprite = pauseImage;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            playPauseButton.image.sprite = playImage;
+        }
+        
+    }
+}
